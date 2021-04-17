b3f7813e1577c45070a3a19cbf63b5eaa05f2f83
The Ninja <69609060+samuraininja360@users.noreply.github.com>
2021-04-17 21:06:07 +1200

    Created math.js

---
+++ math.js
@@ -0,0 +1,15 @@
+function gcd(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+
+console.log(gcd(12, 13))
+console.log(gcd(9, 3))
